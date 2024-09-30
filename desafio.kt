f5b2554618a63e5130e3147b31377e23cedee56f
agpsl <agnaldo.silva.mg@gmail.com>
2024-09-30 14:28:18 -0300

    Solução do desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,95 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String) {
+    override fun toString(): String {
+        return nome
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60) {
+    override fun toString(): String {
+        return "Nome: $nome com duração de $duracao minutos"
+    }
+}
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun getFormacao(): String {
+        return """
+				|Curso: $nome
+				|Conteudos: 
+				|	${getConteudos()}
+				|Nível: ${getNivel()}
+        """.trimMargin("|")
+    }
+
+    fun getConteudos(): String {
+        return conteudos.joinToString("\n")
+    }
+
+    fun getInscritos(): String {
+        return inscritos.joinToString(", ")
+    }
+
+    fun getNivel(): String {
+        val nivelToString = when(nivel) {
+            Nivel.BASICO -> "Básico"
+            Nivel.INTERMEDIARIO -> "Intermediário"
+            Nivel.DIFICIL -> "Difícil"
+            else -> "Desconhecido"
+        }
+        return nivelToString
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    operator fun Int.times(str: String) = str.repeat(this)
+
+    // Inicializa alunos
+    val joao = Usuario("João")
+    val jose = Usuario("José")
+    val maria = Usuario("Maria")
+
+    // Cria conteúdos e formação de Kotlin básico
+    val formacaoKotlin = Formacao("Kotlin Básico",
+        listOf(
+            ConteudoEducacional("Variáveis no Kotlin", 120),
+            ConteudoEducacional("Estruturas de controle no Kotlin", 180),
+            ConteudoEducacional("Orientação a objetos no Kotlin", 240)
+        ),
+        Nivel.BASICO
+    )
+
+    // Cria conteúdos e formação de Java básico
+    val formacaoJava = Formacao("Java Básico",
+        listOf(
+            ConteudoEducacional("Variáveis no Java", 120),
+            ConteudoEducacional("Estruturas de controle no Java", 180),
+            ConteudoEducacional("Orientação a objetos no Java", 240)
+        ),
+        Nivel.BASICO
+    )
+
+    // Verifica cursos
+    println(formacaoKotlin.getFormacao())
+    println(80 * "-")
+    println(formacaoJava.getFormacao())
+    println()
+
+    // Inscreve alunos na formação
+    formacaoKotlin.matricular(joao)
+    formacaoKotlin.matricular(jose)
+    formacaoJava.matricular(maria)
+
+    // Verifica usuários inscritos
+    println("Matriculados na formação Kotlin: ${formacaoKotlin.getInscritos()}")
+    println(80 * "-")
+    println("Matriculados na formação Java: ${formacaoJava.getInscritos()}")
 }
